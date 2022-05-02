fbfda77dc80f9345d7bd4cc77581f8ae9def29ae
TuongNguyen <s3818196@rmit.edu.vn>
2022-05-02 16:20:44 +0700

    customer entity validation

--- server/src/customer/customer.entity.ts
+++ server/src/customer/customer.entity.ts
@@ -1,27 +1,31 @@
-import { IsString } from "class-validator";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { IsEmail, IsString } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Customer {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({name: 'customer_name'})
-    @IsString()
-    customerName: string;
+  @Column({ name: 'customer_name' })
+  @IsString()
+  customerName: string;
 
-    @Column({name: 'customer_type'})
-    customerType: string;
+  @Column({ name: 'customer_type' })
+  @IsString()
+  customerType: string;
 
-    @Column({name: 'customer_contact'})
-    customerContact: string;
+  @Column({ name: 'customer_contact' })
+  @IsString()
+  customerContact: string;
 
-    @Column({name: 'customer_phone'})
-    customerPhone: string;
+  @Column({ name: 'customer_phone' })
+  @IsString()
+  customerPhone: string;
 
-    @Column({name: 'customer_email'})
-    customerEmail: string;
+  @Column({ name: 'customer_email' })
+  @IsEmail()
+  customerEmail: string;
 
-    @Column({name: 'customer_note'})
-    customerNote: string;
+  @Column({ name: 'customer_note', nullable: true })
+  customerNote: string;
 }
