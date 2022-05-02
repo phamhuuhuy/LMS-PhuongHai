9e29ec087f79f02c93f86681744c5a9cb3353601
nguyenquochuy <s3818764@rmit.edu.vn>
2022-05-02 16:51:13 +0700

    update customer

--- server/src/customer/customer.controller.ts
+++ server/src/customer/customer.controller.ts
@@ -1,24 +1,40 @@
-import { Body, Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { Customer } from './customer.entity';
 import { CustomerService } from './customer.service';
+import { UpdateCustomer } from './dto';
 
 @Controller('customer')
 export class CustomerController {
-    constructor(private readonly customerService: CustomerService){}
+  constructor(private readonly customerService: CustomerService) {}
 
-    @Get('/')
-    async getAll() {
-        return await this.customerService.getAll();
-    }
+  @Get('/')
+  async getAll() {
+    return await this.customerService.getAll();
+  }
 
-    @Post('/')
-    async createCustomer(@Body() customer: Customer){
-        return await this.customerService.create(customer);
-    }
+  @Post('/')
+  async createCustomer(@Body() customer: Customer) {
+    return await this.customerService.create(customer);
+  }
 
-    @Get('/:uuid')
-    async getOne(@Param('uuid', new ParseUUIDPipe()) uuid: string) {
-        return await this.customerService.getOne(uuid);
-    }
+  @Get('/:uuid')
+  async getOne(@Param('uuid', new ParseUUIDPipe()) uuid: string) {
+    return await this.customerService.getOne(uuid);
+  }
 
+  @Patch('/:uuid')
+  updateCustomer(
+    @Param('uuid', new ParseUUIDPipe()) uuid: string,
+    @Body() customer: UpdateCustomer,
+  ) {
+    return this.customerService.updateCustomer(uuid, customer);
+  }
 }
--- server/src/customer/customer.service.ts
+++ server/src/customer/customer.service.ts
@@ -1,27 +1,52 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Customer } from './customer.entity';
+import { UpdateCustomer } from './dto';
 
 @Injectable()
 export class CustomerService {
-    constructor(
-        @Inject('CUSTOMER_REPOSITORY')
-        private customerRepository: Repository<Customer>,
-      ) {}
-      
-      async getAll(): Promise<Customer[]> {
-        return await this.customerRepository.find();
-      }
+  constructor(
+    @Inject('CUSTOMER_REPOSITORY')
+    private customerRepository: Repository<Customer>,
+  ) {}
 
-      async create(newCustomer: Customer): Promise<Customer>{
-        return await this.customerRepository.save(newCustomer);
-      }
+  async getAll(): Promise<Customer[]> {
+    return await this.customerRepository.find();
+  }
 
-      async getOne(uuid: string): Promise<Customer> {
-        return await this.customerRepository.findOne({
-          where: {
-            id: uuid
-          }
-        });
-      }
+  async create(newCustomer: Customer): Promise<Customer> {
+    return await this.customerRepository.save(newCustomer);
+  }
+
+  async getOne(uuid: string): Promise<Customer> {
+    return await this.customerRepository.findOne({
+      where: {
+        id: uuid,
+      },
+    });
+  }
+
+  async updateCustomer(
+    uuid: string,
+    updatedCustomer: UpdateCustomer,
+  ): Promise<Customer> {
+    const customerFound = await this.customerRepository.findOne({
+      where: {
+        id: uuid,
+      },
+    });
+    if (!customerFound) {
+      throw new NotFoundException('customer id is not exist');
+    }
+    const customer = await this.customerRepository.save({
+      ...customerFound,
+      ...updatedCustomer,
+    });
+    return customer;
+  }
 }
