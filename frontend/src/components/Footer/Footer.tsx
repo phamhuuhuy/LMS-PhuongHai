06662de7f8b94317da4dcc763e271eff395fff9c
TuongNguyen <s3818196@rmit.edu.vn>
2022-05-05 23:15:45 +0700

    format code

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,52 +1,62 @@
-import { Box, Container, Typography } from '@mui/material'
-import Grid from '@mui/material/Grid'
-import React from 'react'
+import { Box, Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import React from "react";
 
 const data = [
-    {
-        name: 'Nhận mẫu',
-        index: 5
-    },
-    {
-        name: 'Hóa lý',
-        index: 50
-    },
-    {
-        name: 'Vi sinh',
-        index: 99
-    },
-    {
-        name: 'Phòng cơ',
-        index: 12
-    },
-    {
-        name: 'Hóa Phân tích',
-        index: 1
-    },
-]
+  {
+    name: "Nhận mẫu",
+    index: 5,
+  },
+  {
+    name: "Hóa lý",
+    index: 50,
+  },
+  {
+    name: "Vi sinh",
+    index: 99,
+  },
+  {
+    name: "Phòng cơ",
+    index: 12,
+  },
+  {
+    name: "Hóa Phân tích",
+    index: 1,
+  },
+];
 
 const chooseColor = (index: Number): string => {
-    console.log(index);
-    if (index < 10) return '#00ff00'
-    if (index < 20) return '#FFA600'
-    if (index < 70) return 'red'
-    return '#6a0dad'
-}
+  console.log(index);
+  if (index < 10) return "#00ff00";
+  if (index < 20) return "#FFA600";
+  if (index < 70) return "red";
+  return "#6a0dad";
+};
 
 const Footer = () => {
-    return (
-        <Container maxWidth="lg" style={{ height: '100%' }}>
-            <Grid container spacing={2}>
-                {data.map(item =>
-                    <Grid item xs={2}>
-                        <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: chooseColor(item.index) }}>
-                            <Typography style={{ color: 'white' }}>{item.name}</Typography>
-                            <Typography style={{ color: 'white' }}>IQA {item.index}</Typography>
-                        </Box>
-                    </Grid>)}
-            </Grid>
-        </Container >
-    )
-}
+  return (
+    <Container maxWidth="lg" style={{ height: "100%" }}>
+      <Grid container spacing={2}>
+        {data.map((item) => (
+          <Grid item xs={2}>
+            <Box
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                backgroundColor: chooseColor(item.index),
+              }}
+            >
+              <Typography style={{ color: "white" }}>{item.name}</Typography>
+              <Typography style={{ color: "white" }}>
+                IQA {item.index}
+              </Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
 
-export default Footer
+export default Footer;
