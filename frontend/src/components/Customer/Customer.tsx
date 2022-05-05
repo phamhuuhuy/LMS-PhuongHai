d0145a44ba12a91919655be62c0341ca16937474
nguyenquochuy <s3818764@rmit.edu.vn>
2022-05-05 23:19:27 +0700

    delete order file

--- frontend/src/components/Customer/Customer.tsx
+++ frontend/src/components/Customer/Customer.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Orders from './Orders';
-import CustomerTable from './CustomerTable';
+import React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import CustomerTable from "./CustomerTable";
 
 const Customer = () => {
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }} style={{ height: '100%', overflow: 'scroll' }}>
+    <Container
+      maxWidth="lg"
+      sx={{ mt: 4, mb: 4 }}
+      style={{ height: "100%", overflow: "scroll" }}
+    >
       <Grid container spacing={3}>
         {/* Recent Orders */}
         <Grid item xs={12}>
-          <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-            {/* <Orders /> */}
+          <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
             <CustomerTable />
           </Paper>
         </Grid>
       </Grid>
     </Container>
-  )
-}
+  );
+};
 
-export default Customer
+export default Customer;
