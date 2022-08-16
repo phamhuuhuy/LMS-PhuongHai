beecd103bb9be75b41fc5c6c644f67b95736c3c8
Huy <s3825059@rmit.edu.vn>
2022-08-16 22:58:50 +0700

    remove unused file

--- frontend/src/components/System/Method/Method.type.ts
+++
@@ -1,9 +0,0 @@
-export type Method = {
-  methodTargets?: string;
-  methodName?: string;
-  methodDetail?: string;
-  methodScope?: string;
-  methodTime?: string;
-  labName?: string;
-  methodFileUrl?: string;
-};
